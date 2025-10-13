73e5efa1de7c35f52220ba37a4aa2f09077a9064
anjaliyadav8787 <anjaliyaduvanshiy@gmail.com>
2025-10-13 16:39:47 +0530

    Updated home page image animation and hover effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,11 @@
-// import React from 'react'
-// import Typed from 'typed.js'
-// import {motion} from "motion/react"
-// const Home = () => {
-//   const el = React.useRef(null);
-//   React.useEffect(() => {
-//     const typed = new Typed(el.current, {
-//       strings: ['Frontent developer '],
-//       typeSpeed: 100,
-//       backSpeed:50,
-//       loop:true
-//     });
-//     return () => {
-//       typed.destroy();
-//     };
-//   }, []);
-// return (
-//     <div id='main' className=' bg-transparent relative w-[100%] lg:w-full h-[90vh] md:w-[100%] overflow-hidden  '  >
-//       <div className=' bg-no-repeat absolute inset-0 z-0 bg-cover lg:h-[100vh]  bg-[url(https://static.vecteezy.com/system/resources/thumbnails/008/800/866/small_2x/global-connection-and-the-internet-network-modernization-in-smart-city-ai-cloud-internet-background-free-video.jpg)]'></div>
-//       {
-//         Array(20).fill('a').map((ele,i)=>{  
-//           return <motion.div initial={{left:window.innerWidth*Math.random(),top:window.innerHeight*Math.random()}} transition={{duration:Math.random()*4+2, repeat:Infinity,ease:'easeInOut'}} animate={{translateX:[0,Math.random()*30,0],translateY:[0,Math.random()*30,0]}} className='w-[8px] h-[8px] rounded-[100%] bg-gray-600 absolute z-10'>
-//           </motion.div>
-//         })
-//       }
-// <div id='homepage' className=' flex flex-col justify-center items-center relative   w-full h-[90vh] bg-black/50 backdrop-blur-md backdrop-saturate-200
-//   border border-white/20 
-//  shadow-xl shadow-white/10 '>
-//       <div className='flex items-center flex-col text-white lg:pb-10 '>
-//       <h1 className='lg:text-7xl  md:text-7xl text-4xl xl:text-8xl '>I'm Anjali yadav</h1>
-//       <h3 className='lg:text-5xl xl:text-6xl text-3xl mt-7'>I'm  <span ref={el} /></h3></div>
-//       </div>
-//     </div>
-//   )
-// }
-// export default Home
+
 
 import React, { useEffect, useRef } from "react";
 import Typed from "typed.js";
 import { motion } from "motion/react";
 import { Link } from "react-router-dom";
-
+import myimage from '../assets/cf8fa671-7fec-4de6-bfb5-a791dedd6c75.png'
+import Tilt from 'react-parallax-tilt';
 const Home = () => {
   const el = useRef(null);
 
@@ -62,13 +28,13 @@ const Home = () => {
   return (
     <div
       id="home"
-      className="relative w-full min-h-screen flex items-center justify-center bg-black"
+      className="relative w-full min-h-screen flex items-center justify-around bg-black"
     >
       {/* Background Image */}
          <div className=' bg-no-repeat absolute inset-0 z-0 bg-cover lg:h-[100vh]  bg-[url(https://static.vecteezy.com/system/resources/thumbnails/008/800/866/small_2x/global-connection-and-the-internet-network-modernization-in-smart-city-ai-cloud-internet-background-free-video.jpg)]'></div>
 
       {/* Floating particles */}
-      <div className="absolute inset-0 overflow-x-hidden ">
+      <div className="absolute inset-0 overflow-x-hidden  ">
         {Array(25)
           .fill(0)
           .map((_, i) => (
@@ -101,7 +67,7 @@ const Home = () => {
           initial={{ y: -50, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1 }}
-          className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-white"
+          className="text-3xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-7xl font-bold text-white"
         >
           Hi, I'm <span className="text-cyan-400 font-semibold">Anjali Yadav</span>
         </motion.h1>
@@ -136,6 +102,47 @@ const Home = () => {
           </Link>
         </motion.div>
       </div>
+    {/* <div className="relative w-[350px] h-[350px] rounded-full border-2 border-teal-700 overflow-hidden">
+  <img
+    src={myimage}
+    alt=""
+    className="w-full h-full rounded-full object-cover
+               shadow-[0_0_20px_rgba(0,128,255,0.7),
+                       0_0_40px_rgba(0,128,255,0.5),
+                       0_0_60px_rgba(0,128,255,0.3)]
+               transition-transform duration-300 ease-in-out
+               hover:scale-105"
+  /> 
+ </div>  */}
+
+ <div className="relative flex justify-center mt-10">
+  <Tilt
+    className="rounded-full border-2 border-cyan-500 shadow-[0_0_20px_rgba(0,255,255,0.7),0_0_40px_rgba(0,255,255,0.5),0_0_60px_rgba(0,255,255,0.3)]"
+    tiltMaxAngleX={25}
+    tiltMaxAngleY={25}
+    perspective={1000}
+    scale={1.1}
+    transitionSpeed={1000}
+    gyroscope={true}
+  >
+    <motion.img
+      src={myimage}
+      alt="Anjali"
+      className="w-[300px] md:w-[400px] lg:w-[450px] h-[300px] md:h-[400px] lg:h-[450px] rounded-full object-cover border-2 border-cyan-400 shadow-[0_0_30px_rgba(0,255,255,0.5)]"
+      initial={{ opacity: 0, scale: 0.5, rotate: -10 }}
+      animate={{ opacity: 1, scale: 1, rotate: 0 }}
+      transition={{ duration: 1.5, ease: "easeOut" }}
+      whileHover={{
+        scale: 1.1,
+        rotate: [0, 3, -3, 0],
+        boxShadow: "0 0 40px rgba(0,255,255,0.8), 0 0 60px rgba(0,255,255,0.6)",
+      }}
+    />
+  </Tilt>
+</div>
+
+ 
+
     </div>
   );
 };
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -261,88 +261,92 @@
 // export default Portfolio
 
 
-
-
-
 import React from 'react'
 import img from '../assets/WhatsApp-image .jpg'
 import img2 from '../assets/Screenshot.png'
 import img3 from '../assets/clockImg.png'
 import img4 from '../assets/Untitled.png'
+import img5 from '../assets/Untitlebookhub.png'
+
 
 
 const Portfolio = () => {
   return (
-    <div className='2xl:px-[18px] bg-gray-700 sm:px-2 w-full min-h-screen py-10'>
+    <div className='2xl:px-[18px] bg-gray-900 sm:px-2 w-full min-h-screen py-10'>
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 w-[85%] m-auto gap-8'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 w-[100%] px-12 m-auto gap-3 '>
 
         {/* E-Commerce Project */}
-        <div className='border border-gray-100 shadow-2xl sm:w-[300px] sm:h-[800px] lg:w-[400px] xl:w-[400px] xl:min-h-[800px] sm:py-4 lg:py-4 xl:py-4 px-8 font-medium bg-gray-100 md:w-[256px] md:h-[580px] md:px-5 md:py-2 mt-5 h-[800px] w-[390px] m-auto'>
-          <img src={img} alt="" />
-          <h1 className='font-bold text-2xl md:text-[10px] lg:text-2xl xl:text-2xl font-serif mt-2'>1 Title:- <span>E-Commerce Project</span></h1>
-          <h1 className='mt-4 text-2xl md:text-2xl sm:text-[20px]'>2 Description:- </h1>
-          <span className='md:text-[10px] xl:text-[15px] xl:w-[380px] md:line-clamp-6 md:w-[150px] xl:px-2 sm:text-[14px]'>
+        <div className=' shadow-2xl w-[390px] h-[530px] bg-black text-white px-3 py-5 '>
+          <img src={img} alt="" className='px-2 pb-3' />
+          <h1 className='font-bold text-[18px] '>1 Title:- <span>E-Commerce Project</span></h1>
+          <h1 className='mt-4 text-[15px] font-bold'>2 Description:- </h1>
+          <span className='text-[13px] font-semibold '>
             A fully responsive e-commerce frontend with smooth animations, interactive product cards, and a clean shopping experience.
           </span>
-
-          <h1 className='mt-4 text-2xl md:text-[15px] sm:text-[20px]'>3 Features:- </h1>
-          <span className='md:text-[10px] xl:text-[15px] xl:w-[450px] xl:px-2 sm:text-[14px]'>Responsive Design (mobile-first approach)</span>
-          <h3 className='md:text-[10px] xl:text-[15px] xl:w-[450px] xl:px-2 sm:text-[14px]'>Search Bar with Suggestions</h3>
-          <h3 className='md:text-[10px] xl:text-[15px] xl:w-[450px] xl:px-2 sm:text-[14px]'>State Management (Context API)</h3>
-          <h3 className='md:text-[10px] xl:text-[15px] xl:w-[380px] xl:px-2 sm:text-[14px]'>Data Fetching from API (dummyJSON, fakestoreapi, etc.)</h3>
-          <h3 className='md:text-[10px] xl:text-[15px] xl:w-[450px] xl:px-2 sm:text-[14px]'>Add To Cart</h3>
-          <h3 className='md:text-[10px] xl:text-[15px] xl:w-[450px] xl:px-2 sm:text-[14px]'>View To Cart</h3>
-
-          <h1 className='mt-4 text-2xl md:text-[15px] sm:text-[20px]'>4 Tech Stack</h1>
-          <h3 className='md:text-[10px] xl:text-[15px] xl:w-[450px] xl:px-2 sm:text-[14px]'>React.js – Component-based architecture</h3>
-          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[450px] xl:px-2'>Tailwind CSS / SCSS – Styling</h3>
-          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[450px] xl:px-2'>Context API – State management</h3>
-          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[450px] xl:px-2'>Swiper.js – Carousels for product banners</h3>
-          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[450px] xl:px-2'>React Icons / Material UI – Icons</h3>
-          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[380px] xl:px-2'> Vercel Live Demo :- <a href="https://e-commerceproject77.vercel.app/" target="_blank" className="text-blue-700 font-semibold text-[18px]">https://e-commerceproject77.vercel.app/</a></h3>
-           
+          <h1 className='mt-2 text-[15px] font-bold '>3 Features:- </h1>
+          <h3 className='text-[12px] font-semibold '>Search Bar with Suggestions,Add To Cart,View To Cart,Delete Item,Automatice Calculate Amount </h3>
+          <h1 className='mt-2 text-[15px] font-bold '>4 Tech Stack</h1>
+          <h3 className='text-[12px] font-semibold '>React.js – Component-based architecture,Tailwind CSS,Context API,Swiper.js,React Icons</h3>
+          <h3 className='text-[15px] font-bold '> Vercel Live Demo :- <a href="https://e-commerceproject77.vercel.app/" target="_blank" className="text-blue-700 font-semibold text-[12px]">https://e-commerceproject77.vercel.app/</a></h3>          
         </div>
 
+
+            <div className=' shadow-2xl w-[390px] h-[530px] bg-black text-white px-3 py-5 '>
+          <img src={img5} alt="" className='px-2 pb-3' />
+          <h1 className='font-bold text-[18px] '>1 Title:- <span>Book-Hub Project</span></h1>
+          <h1 className='mt-4 text-[15px] font-bold'>2 Description:- </h1>
+          <span className='text-[13px] font-semibold '>
+            A fully responsive Book-Hub frontend with smooth animations, interactive product cards, and a clean shopping experience.
+          </span>
+          <h1 className='mt-2 text-[15px] font-bold '>3 Features:- </h1>
+          <h3 className='text-[12px] font-semibold '>Search Bar with Suggestions,Explore More,Add To Cart,Buy Now,Filter </h3>
+          <h1 className='mt-2 text-[15px] font-bold '>4 Tech Stack</h1>
+          <h3 className='text-[12px] font-semibold '>React.js – Component-based architecture,Tailwind CSS,Context API,Swiper.js,React Icons</h3>
+          <h3 className='text-[15px] font-bold '> Vercel Live Demo :- <a href="https://book-hubproject.vercel.app" target="_blank" className="text-blue-700 font-semibold text-[12px]">https://book-hubproject.vercel.app/</a></h3>          
+        </div>
+
+
+         
         {/* SundownStudio Project */}
-        <div className='border border-gray-100 shadow-2xl w-[390px] h-[550px] sm:w-[300px] sm:h-[800px] md:w-[256px] md:h-[580px] xl:w-[400px] xl:h-[800px] font-medium bg-gray-100 py-3 px-8 m-auto mt-5'>
-          <img src={img2} alt="" />
-          <h1 className='font-bold xl:text-[23px] font-serif mt-2 md:text-[10px]'>1 Title:- <span>SundownStudio Project</span></h1>
-          <h1 className='mt-4 text-2xl md:text-[15px] sm:text-[20px]'>2 Description:- </h1>
-          <span className='xl:text-[15px] xl:w-[380px] md:text-[12px] md:line-clamp-5 md:w-[200px] sm:text-[14px]'>
+        <div className='shadow-2xl w-[390px] h-[530px] bg-black text-white px-3 py-5'>
+          <img src={img2} alt="" className='px-2 pb-3' />
+          <h1 className='font-bold text-[18px]'>1 Title:- <span>SundownStudio Project</span></h1>
+          <h1 className='mt-4 text-[15px] font-bold'>2 Description:- </h1>
+          <span className='text-[13px] font-semibold'>
             A fully responsive SundownStudio Project frontend with smooth animations, interactive Project
           </span>
-          <h1 className='mt-4 text-2xl md:text-[12px] sm:text-[20px]'>4 Tech Stack</h1>
-          <h3 className='md:text-[12px] xl:text-[15px] xl:px-2 sm:text-[15px]'>CSS – Styling</h3>
-          <h3 className='md:text-[12px] xl:text-[15px] xl:px-2 sm:text-[15px]'>Swiper.js – Carousels for project banners</h3>
-          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[380px] xl:px-2'> Vercel Live Demo :- <a href="  https://sundownstudio-beta.vercel.app/" target="_blank" className="text-blue-700 font-semibold text-[18px]">  https://sundownstudio-beta.vercel.app/</a></h3>
+          <h1 className='mt-1  text-[15px] font-bold'>4 Tech Stack</h1>
+          <h3 className='text-[12px] font-semibold '>CSS – Styling,CSS,Animation</h3>
+          
+          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[380px] font-bold'> Vercel Live Demo :- <a href="  https://sundownstudio-beta.vercel.app/" target="_blank" className="text-blue-700 font-semibold text-[12px]">  https://sundownstudio-beta.vercel.app/</a></h3>
         
         </div>
 
         {/* Alarm Clock Project */}
-        <div className='border border-gray-100 shadow-2xl w-[390px] sm:w-[300px] md:w-[256px] md:h-[580px] xl:w-[400px] xl:h-[800px] font-medium bg-gray-100 py-1 px-8 m-auto h-[700px] mt-5'>
-          <img src={img3} alt="" className='sm:w-[300px] sm:h-[250px] m-auto sm:py-5' />
-          <h1 className='font-bold text-1xl font-serif mt-2 md:text-[10px] sm:text-[15px] xl:text-2xl'>1 Title:- <span>Alarm Clock Project</span></h1>
-          <h1 className='mt-4 text-2xl md:text-[18px] sm:text-[20px] sm:mt-1 xl:text-2xl'>2 Description:- </h1>
-          <span className='md:text-[10px] md:line-clamp-4 xl:text-[15px] xl:px-2 sm:text-[14px]'>
+        <div className='shadow-2xl w-[390px] h-[530px] bg-black text-white px-3 py-5'>
+          <img src={img3} alt="" className='sm:w-[300px] sm:h-[250px] m-auto sm:py-5 px-2 pb-3 ' />
+          <h1 className='mt-4 text-[15px] font-bold'>1 Title:- <span>Alarm Clock Project</span></h1>
+          <h1 className='mt-2 text-[15px] font-bold'>2 Description:- </h1>
+          <span className='text-[13px] font-semibold'>
             A fully responsive Alarm Clock Project frontend Project By Using HTML, CSS, JS
           </span>
-          <h1 className='mt-4 text-2xl md:text-[18px] sm:text-[20px] sm:mt-1 xl:text-2xl'>4 Tech Stack</h1>
-          <h3 className='md:text-[10px] xl:text-[15px] xl:px-2 sm:text-[14px]'>CSS – Styling</h3>
-          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[380px] xl:px-2'> Vercel Live Demo :- <a href="  https://alarmclock-woad.vercel.app/" target="_blank" className="text-blue-700 font-semibold text-[18px]">  https://https://alarmclock-woad.vercel.app/</a></h3>
+          <h1 className='mt-2 text-[15px] font-bold'>4 Tech Stack</h1>
+          <h3 className='text-[13px] font-semibold'>CSS – Styling</h3>
+          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[380px]   font-bold'> Vercel Live Demo :- <a href="  https://alarmclock-woad.vercel.app/" target="_blank" className="text-blue-700 font-semibold text-[11px]">  https://https://alarmclock-woad.vercel.app/</a></h3>
         </div>
   {/* *********************************************************************************** */}
 
-<div className='border border-gray-100 shadow-2xl w-[390px] sm:w-[300px] md:w-[256px] md:h-[580px] xl:w-[400px] xl:h-[800px] font-medium bg-gray-100 py-1 px-8 m-auto h-[700px] mt-5'>
-          <img src={img4} alt="" className='sm:w-[300px] sm:h-[250px] m-auto sm:py-5' />
-          <h1 className='font-bold text-1xl font-serif mt-2 md:text-[10px] sm:text-[15px] xl:text-2xl'>1 Title:- <span>E-commerce Project</span></h1>
-          <h1 className='mt-4 text-2xl md:text-[18px] sm:text-[20px] sm:mt-1 xl:text-2xl'>2 Description:- </h1>
-          <span className='md:text-[10px] md:line-clamp-4 xl:text-[15px] xl:px-2 sm:text-[14px]'>
+<div className='shadow-2xl w-[390px] h-[530px] bg-black text-white px-3 py-5'>
+          <img src={img4} alt="" className='sm:w-[300px] sm:h-[250px] m-auto sm:py-5 px-2 pb-3' />
+          <h1 className='mt-4 text-[15px] font-bold'>1 Title:- <span>E-commerce Project</span></h1>
+          <h1 className='mt-2 text-[15px] font-bold'>2 Description:- </h1>
+          <span className='text-[13px] font-semibold'>
             A fully responsive E-commerce Project frontend Project By Using HTML, CSS, JS
           </span>
-          <h1 className='mt-4 text-2xl md:text-[18px] sm:text-[20px] sm:mt-1 xl:text-2xl'>4 Tech Stack</h1>
-          <h3 className='md:text-[10px] xl:text-[15px] xl:px-2 sm:text-[14px]'>CSS – Styling</h3>
-          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[380px] xl:px-2'> Vercel Live Demo :- <a href="  https://ecommerce-project-five-gamma.vercel.app/" target="_blank" className="text-blue-700 font-semibold text-[18px]"> https://ecommerce-project-five-gamma.vercel.app/</a></h3>
+          <h1 className='mt-2 text-[15px] font-bold'>4 Tech Stack</h1>
+          <h3 className='text-[13px] font-semibold'>CSS – Styling</h3>
+          <h3 className='md:text-[10px] sm:text-[14px] xl:text-[15px] xl:w-[380px]  font-semibold'> Vercel Live Demo :- <a href="  https://ecommerce-project-five-gamma.vercel.app/" target="_blank" className="text-blue-700 font-semibold text-[10px]"> https://ecommerce-project-five-gamma.vercel.app/</a></h3>
         </div>
 
       </div>
